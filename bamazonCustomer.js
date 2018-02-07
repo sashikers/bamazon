00b6fd9b7bd7ece054f02f0a009003d9af89960e
Sasha <sashikers@gmail.com>
2018-02-06 20:07:22 -0800

    update newquantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,24 +13,27 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
 	if (err) throw err;
 	displayInventory();
-
 	customerChoice();
 });
 
+// displays the current inventory
 function displayInventory() {
-	connection.query("SELECT item_id, product_name, price, department_name FROM products", function(err, results) {
+	// queries the columns to be displayed
+	connection.query("SELECT item_id, product_name, price, department_name, stock_quantity FROM products", function(err, results) {
 		if (err) throw err;
 
-		console.log("==================INVENTORY==================");
+		// puts it into a palatable format
+		console.log("=============================INVENTORY=============================");
 		for (var i = 0; i < results.length; i++) {
-			console.log(results[i].item_id + ": " + results[i].product_name + " ($" + results[i].price + ", " + results[i].department_name + ")");
+			console.log(results[i].item_id + ": " + results[i].product_name + " ($" + results[i].price + ", " + results[i].department_name + ", " + results[i].stock_quantity + " left)");
 
 		}
 
-		console.log("===============END OF INVENTORY==============");
+		console.log("==========================END OF INVENTORY=========================");
 	});
 }
 
+// prompts the user for choices (which item to buy, quantity)
 function customerChoice() {
 	connection.query("SELECT * FROM products", function(err, results) {
 		if (err) throw err;
@@ -40,20 +43,17 @@ function customerChoice() {
 				{
 					name: "pickAnItem",
 					type: "input",
-					// choices: function() {
-						// var choiceArray = [];
-						// for (var j = 0; j < results.length; j++) {
-						// 	choiceArray.push(results[j].product_name);
-						// }
-					// 	return choiceArray;
-					// },
 					message: "Which item would you like to buy?",
+					// validates the user input against the database list of user_ids
 					validate: function(value) {
 						var choiceArray = [];
+
+						// creates an array of database entries
 						for (var j = 0; j < results.length; j++) {
 							choiceArray.push(results[j].item_id);
 						}
 
+						// checks if the user answer is present in the choicearray and returns false if user choice is not valid
 						if (choiceArray.indexOf(parseInt(value)) < 0) {
 							return false;
 						}
@@ -70,37 +70,47 @@ function customerChoice() {
 				console.log("answer", answer);
 				var chosenItem;
 
+				// pulls in the database info for selected item 
 				for (var k = 0; k < results.length; k++) {
 					if (results[k].item_id === parseInt(answer.pickAnItem)) {
 						chosenItem = results[k];
-						// console.log("chosenItem", chosenItem);
 					}
 				}
 
+				// checks the selected amount against user amt choice
 				if (chosenItem.stock_quantity < parseInt(answer.pickAQuantity)) {
-					console.log("Insufficient quantity!! Only " + chosenItem.stock_quantity + " units left!");
-					displayInventory();
+					console.log("Insufficient quantity!! Only " + chosenItem.stock_quantity + " units of " + chosenItem.product_name + " left!");
+					// prompts the customer for another choice if inventory is insufficient
 					customerChoice();
 				}
+				else {
+					// console.log("Good buy!");
+
+					var newQuantity = chosenItem.stock_quantity - answer.pickAQuantity;
+					console.log("newQuantity", newQuantity);
+
+					connection.query(
+						"UPDATE products SET ? WHERE ?",
+						[
+							{
+								stock_quantity: newQuantity
+							},
+							{
+								item_id: chosenItem.id
+							}
+						],   
+						function(error) {
+							if (error) throw err;
+							console.log("Good buy!!");
+
+							displayInventory();
+						}
+					);
+				}
+
 
 				// console.log("chosenItem", chosenItem);
 
 			});
 	});
 }
-
-// function customerChoice() {
-// 	inquirer
-// 		.prompt({
-// 			name: "pickAnItem",
-// 			type: "input",
-// 			messages: "Which item would you like to buy?"
-// 			// choices: [1,2,3,4,5,6,7,8,]
-// 		},
-// 		{
-// 			name: "pickAQuantity",
-// 			type: "input",
-// 			messages: "How many would you like to buy?"
-// 		})
-// 		.then();
-// }
