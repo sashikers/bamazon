b3ee39f8a9909fd8dcee647c447513255713c965
Sasha <sashikers@gmail.com>
2018-02-06 19:08:57 -0800

    initial inquiry works

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,15 +13,77 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
 	if (err) throw err;
 	displayInventory();
+
+	customerChoice();
 });
 
 function displayInventory() {
 	connection.query("SELECT item_id, product_name, price, department_name FROM products", function(err, results) {
 		if (err) throw err;
 
-		// console.log(results);
+		console.log("==================INVENTORY==================");
 		for (var i = 0; i < results.length; i++) {
 			console.log(results[i].item_id + ": " + results[i].product_name + " ($" + results[i].price + ", " + results[i].department_name + ")");
+
 		}
+
+		console.log("===============END OF INVENTORY==============");
 	});
 }
+
+function customerChoice() {
+	connection.query("SELECT * FROM products", function(err, results) {
+		if (err) throw err;
+
+		inquirer
+			.prompt([
+				{
+					name: "pickAnItem",
+					type: "input",
+					// choices: function() {
+						// var choiceArray = [];
+						// for (var j = 0; j < results.length; j++) {
+						// 	choiceArray.push(results[j].product_name);
+						// }
+					// 	return choiceArray;
+					// },
+					message: "Which item would you like to buy?",
+					validate: function(value) {
+						var choiceArray = [];
+						for (var j = 0; j < results.length; j++) {
+							choiceArray.push(results[j].item_id);
+						}
+
+						if (choiceArray.indexOf(parseInt(value)) < 0) {
+							return false;
+						}
+						return true;
+					}
+				},
+				{
+					name: "pickAQuantity",
+					type: "input",
+					message: "How many would you like to buy?"
+				}
+			])
+			.then(function(answer) {
+				console.log(answer);
+			});
+	});
+}
+
+// function customerChoice() {
+// 	inquirer
+// 		.prompt({
+// 			name: "pickAnItem",
+// 			type: "input",
+// 			messages: "Which item would you like to buy?"
+// 			// choices: [1,2,3,4,5,6,7,8,]
+// 		},
+// 		{
+// 			name: "pickAQuantity",
+// 			type: "input",
+// 			messages: "How many would you like to buy?"
+// 		})
+// 		.then();
+// }
